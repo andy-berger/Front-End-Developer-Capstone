c64613cf755ebc5d525c7606a02a3d2b8a4ecf0a
Andy Berger <inocid@gmail.com>
2023-03-17 08:55:14 +0100

    Added React files for the different parts of the page

---
+++ littlelemon/src/Header.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+
+const Header = () => {
+    return (
+        <header>
+            <img src={logo} alt="Little Lemon logo" />
+        </header>
+    );
+};
+
+export default Header;
